24801c4651aa1affa041057a44a07980b7a95f48
rjtj2007 <teacherman1@gmail.com>
2018-02-17 18:27:31 -0800

    working on transform functionality

--- lab-ryan/index.js
+++ lab-ryan/index.js
@@ -1,36 +1,70 @@
 "use strict";
 
 const fs = require('fs');
-const trasnform = require('../lib/transform.js');
+// const trasnform = require('./lib/transform.js');
 //provides access to node on command line interface (CLI)
-// if(process.argv.includes('hello')) console.log('world');
+if (process.argv.includes('hello')) console.log('world');
 
-fs.readFile(`${__dirname}/test/assets/penguin.bmp`, (err, data) => {
-    if (err) console.log (err);
-    // console.log('data ', data);
-    
-    let bitMap = {
-        header: {
-            type: data.slice(0,2).toString(),
-            fileSize: data.slice(2,6).readUInt16LE(),
-            fileOffset: data.slice(10, 14).readUInt32LE(),
-        },
-        dibHeader: {
-            width: data.slice(18,22).readUInt32LE(),
-            height: data.slice(22,26).readUInt32LE(),
-            bitsPerPX: data.slice(28,30).readUInt16LE(),
-        },
-         pixelArray: data.slice(54).readUInt16LE(),
-    }
-        // console.log('type ', bitMap.header.type);
-        // console.log('fileSize ', bitMap.header.fileSize);
-        // console.log('fileOffset ', bitMap.header.fileOffset);
-        // console.log('width ', bitMap.dibHeader.width);
-        // console.log('height ', bitMap.dibHeader.height);
-        // console.log('bits per PX ', bitMap.dibHeader.bitsPerPX);
-        // console.log('pixelArray ', bitMap.pixelArray);
-        // console.log('data ', data[10]);
-});
+let inputFilePath = `${__dirname}/test/assets/blob.bmp`;
+let transform = 'mirror';
+let outputFilePath = `${__dirname}/test/assets/newAssets/newBlob.bmp`;
 
-module.exports = {};
-module.exports.bitMap = bitMap;
+function BitmapTransformer(inputFilePath, outputFilePath, transform) {
+
+
+    fs.readFile(`${__dirname}/test/assets/penguin.bmp`, (err, buffer) => {
+        if (err) return console.error(err);
+
+        const bitmap = {
+
+            header: {
+                type: buffer.slice(0, 2).toString(),
+                fileSize: buffer.slice(2, 6).readUInt16LE(),
+                offsetToPixelArray: buffer.slice(10, 14).readUInt32LE(),
+            },
+            dibHeader: {
+                width: buffer.slice(18, 22),
+                height: buffer.slice(22, 26),
+                bitsPerPixel: buffer.slice(28, 30),
+            },
+            pixelArray: buffer.slice(buffer.slice(10, 14).readUInt32LE()),
+        }
+        console.log('bitmap', bitmap);
+
+        const mirrorTransform = (transform) => {
+            if (transform !== undefined) {
+                if (transform === 'mirror') {
+                    let mirrorImage = [];
+                    console.log('bitmap pixel array=', bitmap.pixelArray.length);
+                    for (let i = bitmap.pixelArray.length - 1; i > 0; i--) {
+                        mirrorImage.push(bitmap.pixelArray[i]);
+                    }
+                    console.log('buffer=', buffer);
+
+                    let newBuffer = buffer.slice(0, bitmap.offsetToPixelArray) + mirrorImage;
+                    return newBuffer;
+                }
+                // if (transform === 'grayScale') {
+                //     let mirrorImage = [];
+                //     console.log('bitmap pixel array=', bitmap.pixelArray.length);
+                //     for (let i = bitmap.pixelArray.length - 1; i > 0; i--) {
+                //         mirrorImage.push(bitmap.pixelArray[i]);
+                //     }
+                //     console.log('buffer=', buffer);
+
+                //     let newBuffer = buffer.slice(0, bitmap.offsetToPixelArray) + mirrorImage;
+                //     return newBuffer;
+                // }
+            }
+        }
+        let newBuffer = mirrorTransform(transform);
+
+        fs.writeFile(outputFilePath, newBuffer, err => {
+            if (err) return console.log(err)
+        })
+    })
+}
+BitmapTransformer(inputFilePath, outputFilePath, transform);
+
+// module.exports = {};
+// module.exports.mirrorTransform = mirrorTransform;
