842654f7a46b923579df25b94604b37f868f54d1
rjtj2007 <teacherman1@gmail.com>
2018-02-18 21:18:01 -0800

    refactors not working, back to start, added jest for testing

--- lab-ryan/lib/transform.js
+++ lab-ryan/lib/transform.js
@@ -1,58 +1,81 @@
-'use strict';
-const fs = require('fs');
+// 'use strict';
+// const fs = require('fs');
+// const required = require('../index.js');
 
-//provides access to node on command line interface (CLI)
-// if(process.argv.includes('hello')) console.log('world');
+// //provides access to node on command line interface (CLI)
+// // if(process.argv.includes('hello')) console.log('world');
 
-const required = require('../index.js');
+// function BitmapTransformer(inputFilePath, outputFilePath, transform) {
 
-let transform = () => {
-    for(let i = 54; i < data.length; i = i+50){
-        data[i] = '255';
-    }
-    console.log('transform', data[54]);
-}
-transform();
+//     fs.readFile(`${__dirname}/test/assets/penguin.bmp`, (err, buffer) => {
+//         if (err) return console.error(err);
 
-fs.writeFile(`${__dirname}/test/assets/penguin1.bmp`, data, function(err) {
-    if (err) {
-        console.log(err)
-    }
-        console.log('success')
-})
+//         const bitmap = {
 
-module.exports = {};
-module.exports.transform = transform;
+//             header: {
+//                 type: buffer.slice(0, 2).toString(),
+//                 fileSize: buffer.slice(2, 6).readUInt16LE(),
+//                 offsetToPixelArray: buffer.slice(10, 14).readUInt32LE(),
+//             },
+//             dibHeader: {
+//                 width: buffer.slice(18, 22),
+//                 height: buffer.slice(22, 26),
+//                 bitsPerPixel: buffer.slice(28, 30),
+//             },
+//             pixelArray: buffer.slice(buffer.slice(10, 14).readUInt32LE()),
+//         }
+//         console.log('bitmap', bitmap);
 
-//////////////////////
+//         const mirrorTransform = (transform) => {
+//             if (transform !== undefined) {
+//                 if (transform === 'mirror') {
+//                     let mirrorImage = [];
+//                     console.log('bitmap pixel array=', bitmap.pixelArray.length);
+//                     for (let i = bitmap.pixelArray.length - 1; i > 0; i--) {
+//                         mirrorImage.push(bitmap.pixelArray[i]);
+//                     }
+//                     console.log('buffer=', buffer);
 
-'use strict';
+//                     let newBuffer = buffer.slice(0, bitmap.offsetToPixelArray) + mirrorImage;
+//                     return newBuffer;
+//                 }
+//             }
+//         }
+//         let newBuffer = mirrorTransform(transform);
 
-const fs = require('fs');
+//         fs.writeFile(outputFilePath, newBuffer, err => {
+//             if (err) return console.log(err)
+//         })
+//     })
+// }
+// BitmapTransformer(inputFilePath, outputFilePath, transform);
 
-const transform = {
-  readFile: (file, outputFilePath, callback) => {
-    fs.readFile(file, (err, buffer) => {
-      if (err) return console.error(err);
+// module.exports = {};
+// module.exports.BitmapTransformer = BitmapTransformer;
 
-      callback(outputFilePath, bitmap, buffer);
-    });
-  },
-  
-  greyScale: (outputFilePath, bitmap, buffer) => {
-    let result = [];
-    for (let i = bitmap.pixelArray.length - 1; i > 0; i--) {
-      result.push(bitmap.pixelArray[i]);
-    }
+// const transform = {
+//     readFile: (file, outputFilePath, callback) => {
+//         fs.readFile(file, (err, buffer) => {
+//             if (err) return console.error(err);
 
-    let newBuffer = buffer.slice(0, bitmap.offsetToPixelArray) + result;
+//             callback(outputFilePath, bitmap, buffer);
+//         });
+//     },
 
-    fs.writeFile(outputFilePath, newBuffer, err => {
-      if (err) return console.log(err);
-      console.log(`The file has been saved at ${outputFilePath}`);
-    });
-  }
-};
+//     greyScale: (outputFilePath, bitmap, buffer) => {
+//         let result = [];
+//         for (let i = bitmap.pixelArray.length - 1; i > 0; i--) {
+//             result.push(bitmap.pixelArray[i]);
+//         }
 
-module.exports = {};
-module.exports.transform = transform;
+//         let newBuffer = buffer.slice(0, bitmap.offsetToPixelArray) + result;
+
+//         fs.writeFile(outputFilePath, newBuffer, err => {
+//             if (err) return console.log(err);
+//             console.log(`The file has been saved at ${outputFilePath}`);
+//         });
+//     }
+// };
+
+// module.exports = {};
+// module.exports.transform = transform;
